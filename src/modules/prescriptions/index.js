670c4ba72f0e4a9154af5e1095c294959e77ebba
Muhammad Yasir <anwarmuhammadyasir11@gmail.com>
2025-12-16 17:55:37 +0500

    updated

--- src/modules/prescriptions/index.js
+++ src/modules/prescriptions/index.js
@@ -1,2 +1,9 @@
 export { default as UploadPrescription } from "./pages/UploadPrescription";
 export { default as PrescriptionStatus } from "./pages/PrescriptionStatus";
+
+export { default as PrescriptionCard } from './components/PrescriptionCard';
+export { default as PrescriptionUploadForm } from './components/PrescriptionUploadForm';
+
+export { default as usePrescriptions } from './hooks/usePrescriptions';
+
+export * from './services/prescriptionService';
